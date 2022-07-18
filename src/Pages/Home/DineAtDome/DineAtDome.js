4167c936e51c2740d4066ededf93ffeb3bd61962
Sajid Rahman <sajidjrahman@icloud.com>
2022-07-18 14:49:55 +0200

    first commit

---
+++ src/Pages/Home/DineAtDome/DineAtDome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DineAtDome.css';
+
+const DineAtDome = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DineAtDome;
---
+++ src/Pages/Home/HowItWorks/HowItWorks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HowItWorks.css';
+
+const HowItWorks = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HowItWorks;
